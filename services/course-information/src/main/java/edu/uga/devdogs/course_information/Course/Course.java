243959062f4e6901d628526d352652e68d907b4e
Igor R. <124288137+Ig0rGon@users.noreply.github.com>
2025-01-27 18:32:32 -0500

    bryant and i killed it!

--- services/course-information/src/main/java/edu/uga/devdogs/course_information/Course/Course.java
+++ services/course-information/src/main/java/edu/uga/devdogs/course_information/Course/Course.java
@@ -29,10 +29,22 @@ public class Course implements Serializable {
     @Column(name = "course_id")
     private long courseId;
 
+    private String title;
+
     private String subject;
     private String courseNumber;
     private String title;
     private String department;
+
+    private String courseInfoById;
+
+    private List<CourseSection> courseSectionsById;
+
+    private List<CourseSection> preRequisites;
+
+    private List<CourseSection> coRequisites;
+
+
 
     // Relationships
     @OneToMany(mappedBy = "course")
@@ -86,24 +98,39 @@ public class Course implements Serializable {
         this.prerequisiteCourses = new ArrayList<>();
     }
 
-    public Course(String subject, String courseNumber, String title, String department, List<CourseSection> courseSections) {
+    public Course(String subject, String courseNumber, String title, String department, List<CourseSection> courseSections, 
+    String courseInfoById, List<CourseSection> preRequisites, List<CourseSection> coRequisites) {
+        this.title = title;
         this.subject = subject;
         this.courseNumber = courseNumber;
-        this.title = title;
         this.department = department;
+        this.courseInfoById = courseInfoById;
+        this.preRequisites = preRequisites;
+        this.coRequisites = coRequisites;
+        this.courseSections = courseSections;
+        this.courseDescription = courseDescription;
+        this.athenaTitle = athenaTitle;
+        this.equivalentCourses = equivalentCourses;
+        this.prerequisiteCourses = prerequisiteCourses;
+        this.semesters = semesters;
+        this.gradingSystem = gradingSystem;
+    }
+
+    public Course(long courseId, String subject, String courseNumber, String title, String department, List<CourseSection> courseSections, 
+    String courseInfoById, List<CourseSection> preRequisites, List<CourseSection> coRequisites) {    
+        this.courseId = courseId;
+        this.title = title;
+        this.subject = subject;
+        this.courseNumber = courseNumber;
+        this.department = department;
+        this.courseInfoById = courseInfoById;
+        this.preRequisites = preRequisites;
+        this.coRequisites = coRequisites;
         this.courseSections = courseSections;
         this.equivalentCourses = new ArrayList<>();
         this.prerequisiteCourses = new ArrayList<>();
     }
 
-    public Course(long courseId, String subject, String courseNumber, String title, String department, List<CourseSection> courseSections) {
-        this.courseId = courseId;
-        this.subject = subject;
-        this.courseNumber = courseNumber;
-        this.title = title;
-        this.department = department;
-        this.courseSections = courseSections;
-    }
 
     // Getters and setters
     public long getCourseId() {
@@ -122,9 +149,9 @@ public class Course implements Serializable {
         this.subject = subject;
     }
 
-    public String getCourseNumber() {
-        return courseNumber;
-    }
+                    public String getCourseNumber() {
+                        return courseNumber;
+                    }
 
     public void setCourseNumber(String courseNumber) {
         this.courseNumber = courseNumber;
@@ -143,11 +170,9 @@ public class Course implements Serializable {
     }
 
     public void setDepartment(String department) {
-        this.title = title;
+
         this.department = department;
-        this.courseSections = courseSections;
-        this.equivalentCourses = new ArrayList<>();
-        this.prerequisiteCourses = new ArrayList<>();
+
     }
     
     //constructor for course with id
@@ -160,59 +185,60 @@ public class Course implements Serializable {
         this.courseSections = courseSections;
     }
 
-                    // getters and setters for course
-                    public long getCourseId() {
-                        return courseId;
-                    }
 
-                    public void setCourseId(long courseId) {
-                        this.courseId = courseId;
-                    }
 
-                    public String getSubject() {
-                        return subject;
-                    }
-
-                    public void setSubject(String subject) {
-                        this.subject = subject;
-                    }
-
-                    public String getCourseNumber() {
-                        return courseNumber;
-                    }
-
-                    public void setCourseNumber(String courseNumber) {
-                        this.courseNumber = courseNumber;
-                    }
-
-                    public String getTitle() {
-                        return title;
-                    }
-
-                    public void setTitle(String title) {
-                        this.title = title;
-                    }
-
-                    public String getDepartment() {
-                        return department;
-                    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-                    public List<CourseSection> getCourseSections() {
-                        return courseSections;
-                    }
+                public List<CourseSection> getCourseSections() {
+                    return courseSections;
+                }
 
     public void setCourseSections(List<CourseSection> courseSections) {
         this.courseSections = courseSections;
     }
 
+    public List<CourseSection> getCourseSectionsById(long id) {
+        return courseSectionsById;
+    }
+
+    public List<CourseSection> getPrerequisets(long id) {
+        return preRequisites;
+    }
+
+    public void setPrerequisets(List<CourseSection> preRequisites) {
+        this.preRequisites = preRequisites;
+    }
+
+    public List<CourseSection> getCorequisets(long id) {
+        return coRequisites;
+    }
+
+    public void setCorequisets(List<CourseSection> coRequisites) {
+        this.coRequisites = coRequisites;
+    }
+ 
     @Override
     public String toString() {
-        return "Course [courseId=" + courseId + ", subject=" + subject + ", courseNumber=" + courseNumber + ", title="
-                + title + ", department=" + department + "]";
+        return "Course {" +
+            "courseId=" + courseId + ", " +
+            "title='" + title + '\'' + ", " +
+            "subject='" + subject + '\'' + ", " +
+            "courseNumber='" + courseNumber + '\'' + ", " +
+            "department='" + department + '\'' + ", " +
+            "courseInfoById='" + courseInfoById + '\'' + ", " +
+            "courseSectionsById=" + formatList(courseSectionsById) + ", " +
+            "preRequisites=" + formatList(preRequisites) + ", " +
+            "coRequisites=" + formatList(coRequisites) +
+            '}';
+}
+
+    private String formatList(List<CourseSection> list) {
+        if (list == null || list.isEmpty()) {
+            return "[]";
+        }
+        return list.stream()
+               .map(Object::toString)
+               .reduce((s1, s2) -> s1 + ", " + s2)
+               .map(result -> "[" + result + "]")
+               .orElse("[]");
     }
 
     
